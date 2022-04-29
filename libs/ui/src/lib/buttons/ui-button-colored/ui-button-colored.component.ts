992f71290e98f6df204db006bf5d63170e4b4058
Daniel Rodrigues De Sousa <danielrdesousa@brq.com>
2022-04-28 22:51:31 -0300

    feat: config eslint to empty functions

---
+++ libs/ui/src/lib/buttons/ui-button-colored/ui-button-colored.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-button-colored',
+  templateUrl: './ui-button-colored.component.html',
+  styleUrls: ['./ui-button-colored.component.scss'],
+})
+export class UiButtonColoredComponent  {
+  constructor() {}
+}
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
+import { UiButtonColoredComponent } from './buttons/ui-button-colored/ui-button-colored.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent],
+  declarations: [ButtonComponent, UiButtonColoredComponent],
   exports: [ButtonComponent],
 })
 export class UiModule {}
