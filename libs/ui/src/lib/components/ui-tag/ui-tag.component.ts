252446e64b5878f0f282543b5e84e408b773b53d
Daniel Rodrigues <danielrdesousa@gmail.com>
2022-04-30 15:39:05 -0300

    wip: tag component style

--- libs/ui/src/lib/components/ui-tag/ui-tag.component.ts
+++ libs/ui/src/lib/components/ui-tag/ui-tag.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'ui-ui-tag',
+  selector: 'ui-tag',
   templateUrl: './ui-tag.component.html',
   styleUrls: ['./ui-tag.component.scss'],
 })
 export class UiTagComponent implements OnInit {
+  @Input() type = 'colored';
+  @Input() size!: string;
+  @Input() canDelete = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  remove(event: Event): void {
+    const element = (event.target as Element);
+    const parentElement = element.parentElement;
+
+    parentElement?.remove();
+  }
 }
--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -4,8 +4,16 @@ import { ButtonsModule } from './buttons/buttons.module';
 import { UiTagComponent } from './components/ui-tag/ui-tag.component';
 
 @NgModule({
-  imports: [CommonModule, ButtonsModule],
-  declarations: [UiTagComponent],
-  exports: [ButtonsModule],
+  imports: [
+    CommonModule,
+    ButtonsModule
+  ],
+  declarations: [
+    UiTagComponent
+  ],
+  exports: [
+    ButtonsModule,
+    UiTagComponent
+  ],
 })
 export class UiModule {}
