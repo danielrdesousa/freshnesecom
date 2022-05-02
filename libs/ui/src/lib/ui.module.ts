7ea6369d945787f7704a746de0427a5a89a9cf5f
Daniel Rodrigues <danielrdesousa@gmail.com>
2022-05-01 23:50:03 -0300

    feat: inclusion of the ui-search component

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ButtonsModule } from './buttons/buttons.module';
+import { UiFormModule } from './form/ui-form.module';
 
 import * as fromComponents from './components';
 
 @NgModule({
-  imports: [CommonModule, ButtonsModule],
+  imports: [CommonModule, ButtonsModule, UiFormModule],
   declarations: [...fromComponents.all],
-  exports: [ButtonsModule, ...fromComponents.all],
+  exports: [ButtonsModule, UiFormModule, ...fromComponents.all],
 })
 export class UiModule {}
